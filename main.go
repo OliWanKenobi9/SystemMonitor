347128deff8441de418f7923aa6ea1a051e7f951
OliWan9 <oliver.hitz.7@gmail.com>
2025-12-24 15:09:45 +0100

    Temp Graph & Output

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/exec"
@@ -12,6 +13,7 @@ import (
 	"github.com/shirou/gopsutil/v4/disk"
 	"github.com/shirou/gopsutil/v4/mem"
 	"github.com/shirou/gopsutil/v4/net"
+	"github.com/shirou/gopsutil/v4/sensors"
 )
 
 const (
@@ -19,25 +21,27 @@ const (
 	Bold      = "\033[1m"
 	Underline = "\033[4m"
 
-	Red     = "\033[31m"
-	Green   = "\033[32m"
-	Yellow  = "\033[33m"
-	Blue    = "\033[34m"
-	Magenta = "\033[35m"
-	Cyan    = "\033[36m"
-	White   = "\033[37m"
+	Red         = "\033[31m"
+	Green       = "\033[32m"
+	Yellow      = "\033[33m"
+	Blue        = "\033[34m"
+	Magenta     = "\033[35m"
+	Cyan        = "\033[36m"
+	White       = "\033[37m"
+	ScreenWidth = 36
 ) // ANSI Formatting
 
-func GetData() (*disk.UsageStat, []cpu.InfoStat, []float64, *mem.VirtualMemoryStat, []net.IOCountersStat, error) {
+func GetData() (*disk.UsageStat, []cpu.InfoStat, []float64, *mem.VirtualMemoryStat, []net.IOCountersStat, []sensors.TemperatureStat, error) {
 	var diskUsage *disk.UsageStat
 	var cpuInfo []cpu.InfoStat
 	var cpuPercent []float64
 	var memoryInfo *mem.VirtualMemoryStat
 	var netInfo []net.IOCountersStat
+	var temperatures []sensors.TemperatureStat
 	var err error
 
 	var wg sync.WaitGroup
-	wg.Add(5)
+	wg.Add(6)
 
 	go func() {
 		defer wg.Done()
@@ -64,8 +68,13 @@ func GetData() (*disk.UsageStat, []cpu.InfoStat, []float64, *mem.VirtualMemorySt
 		netInfo, _ = net.IOCounters(true)
 	}()
 
+	go func() {
+		defer wg.Done()
+		temperatures, _ = sensors.TemperaturesWithContext(context.Background())
+	}()
+
 	wg.Wait()
-	return diskUsage, cpuInfo, cpuPercent, memoryInfo, netInfo, err
+	return diskUsage, cpuInfo, cpuPercent, memoryInfo, netInfo, temperatures, err
 }
 
 func ClearScreen() {
@@ -91,7 +100,7 @@ func printValue(value string, cursorTop int, cursorLeft int, screenWidth int) {
 	fmt.Printf("\033[%d;%dH┃\n", cursorTop, screenWidth)
 }
 
-func PrintMenu(diskUsage *disk.UsageStat, cpuInfo []cpu.InfoStat, cpuPercent []float64, memoryInfo *mem.VirtualMemoryStat, netInfo []net.IOCountersStat, err error, BytesRecvDelta float64) {
+func PrintMenu(diskUsage *disk.UsageStat, cpuInfo []cpu.InfoStat, cpuPercent []float64, memoryInfo *mem.VirtualMemoryStat, netInfo []net.IOCountersStat, tempInfo []sensors.TemperatureStat, err error, BytesRecvDelta float64) {
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -101,27 +110,30 @@ func PrintMenu(diskUsage *disk.UsageStat, cpuInfo []cpu.InfoStat, cpuPercent []f
 
 	padding := 0
 	modelLine := ""
-	if len(cpuInfo[0].ModelName) > 19 {
+	if len(cpuInfo[0].ModelName) > ScreenWidth/2+1 {
 		padding = 1
 		modelLine = fmt.Sprintf("┃ %sCPU Model:   %s%s%s%s\n┃              %s",
-			Blue, Reset, cpuInfo[0].ModelName[:18], "-", Reset, cpuInfo[0].ModelName[18:])
+			Blue, Reset, cpuInfo[0].ModelName[:ScreenWidth/2], "-", Reset, cpuInfo[0].ModelName[18:])
 
-		printValue(modelLine, 4, 0, 36)
-		fmt.Printf("\033[5;36H") // Set cursor
+		printValue(modelLine, 4, 0, ScreenWidth)
+		fmt.Printf("\033[5;%sH", ScreenWidth) // Set cursor
 		fmt.Printf("┃")
 	} else {
 		modelLine = fmt.Sprintf("┃ %s%sCPU Model:  %s %s", Blue, Bold, Reset, cpuInfo[0].ModelName)
-		printValue(modelLine, 4, 0, 36)
+		printValue(modelLine, 4, 0, ScreenWidth)
 	}
 
 	cpuLine := fmt.Sprintf("┃ %s%sCPU Used:%s    %s [%.2f%%]", Blue, Bold, Reset, GetProgressBar(int(cpuPercent[0]), 10), cpuPercent[0])
-	printValue(cpuLine, 5+padding, 0, 36)
+	printValue(cpuLine, 5+padding, 0, ScreenWidth)
 
 	diskLine := fmt.Sprintf("┃ %s%sDisk Used:%s   %s [%.2f%%]", Green, Bold, Reset, GetProgressBar(int(diskUsage.UsedPercent), 10), diskUsage.UsedPercent)
-	printValue(diskLine, 6+padding, 0, 36)
+	printValue(diskLine, 6+padding, 0, ScreenWidth)
 
 	memLine := fmt.Sprintf("┃ %s%sMemory Used:%s %s [%.2f%%]", Yellow, Bold, Reset, GetProgressBar(int(memoryInfo.UsedPercent), 10), memoryInfo.UsedPercent)
-	printValue(memLine, 7+padding, 0, 36)
+	printValue(memLine, 7+padding, 0, ScreenWidth)
+
+	tempLine := fmt.Sprintf("┃ %s%sTemps:%s       %s [%.2f%%]", Red, Bold, Reset, GetProgressBar(int(tempInfo[0].Temperature), 10), tempInfo[0].Temperature)
+	printValue(tempLine, 8+padding, 0, ScreenWidth)
 
 	prefixes := [6]string{"B", "KiB", "MiB", "GiB", "TiB", "PiB"}
 	i := 0
@@ -129,11 +141,11 @@ func PrintMenu(diskUsage *disk.UsageStat, cpuInfo []cpu.InfoStat, cpuPercent []f
 		BytesRecvDelta /= 1024
 	}
 	netLine := fmt.Sprintf("┃ %s%sNetwork:%s     %.2f %s", Magenta, Bold, Reset, BytesRecvDelta, prefixes[i])
-	printValue(netLine, 8+padding, 0, 36)
+	printValue(netLine, 9+padding, 0, ScreenWidth)
 
-	fmt.Printf("\033[9;0H")
-	fmt.Printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n")
 	fmt.Printf("\033[10;0H")
+	fmt.Printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n")
+	fmt.Printf("\033[11;0H")
 }
 
 func GetProgressBar(progress int, base int) string {
@@ -153,14 +165,17 @@ func GetProgressBar(progress int, base int) string {
 }
 
 func main() {
-	diskUsage, cpuInfo, cpuPercent, memoryInfo, netInfo, err := GetData()
+	/*
+	 * TODO: Net Graph 0-Max
+	 */
+	diskUsage, cpuInfo, cpuPercent, memoryInfo, netInfo, temperatureInfo, err := GetData()
 	for {
 		BytesRecvLastIt := netInfo[0].BytesRecv
-		diskUsage, cpuInfo, cpuPercent, memoryInfo, netInfo, err = GetData()
+		diskUsage, cpuInfo, cpuPercent, memoryInfo, netInfo, temperatureInfo, err = GetData()
 		BytesRecvDelta := netInfo[0].BytesRecv - BytesRecvLastIt
 
 		ClearScreen()
-		PrintMenu(diskUsage, cpuInfo, cpuPercent, memoryInfo, netInfo, err, float64(BytesRecvDelta))
+		PrintMenu(diskUsage, cpuInfo, cpuPercent, memoryInfo, netInfo, temperatureInfo, err, float64(BytesRecvDelta))
 
 		time.Sleep(2000 * time.Millisecond)
 	}
